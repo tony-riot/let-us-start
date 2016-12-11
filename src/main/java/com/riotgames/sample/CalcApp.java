270704471a10bd52b2d5d2ccdba0749226e3089d
tchi <tchi@riotgames.com>
2016-12-11 15:16:06 +0900

    Modify main class name

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -4,7 +4,7 @@ package com.riotgames.sample;
  * Hello world!
  *
  */
-public class App {
+public class CalcApp {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
